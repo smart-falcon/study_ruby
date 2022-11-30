dd1591d270e74aea9b2e97264327c7a91e737bef
smart-falcon <suzukihourensou.123456789@gmail.com>
2022-11-30 10:36:04 +0900

    上書き

--- 36.rb
+++ 36.rb
@@ -1,1 +1,23 @@
 #クラスから始めよ
+
+=begin
+クラス名は必ず最初の文字は大文字。   
+インスタンス生成時に呼ばれるメソッドはdef initializeと
+定義する。
+この時にクラス外から渡される値をインスタンス内で使うように
+するためにインスタンス変数を作る(@インスタンス変数)   
+=end
+
+class User
+    def initialize(name)
+        @name = name
+    end
+    def sayHi
+        puts "Hi! i am #{@name}"
+    end
+end
+
+tom = User.new("tom")
+tom.sayHi
+endo = User.new("endo")
+endo.sayHi
